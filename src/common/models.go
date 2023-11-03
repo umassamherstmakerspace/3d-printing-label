858a6f123377592b450bd467df5172c1949a5a75
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2023-11-03 18:22:01 -0400

    changed label to add printer and spacing

--- src/common/models.go
+++ src/common/models.go
@@ -2,6 +2,7 @@ package models
 
 type Print struct {
 	HumanName      string `json:"humanName" xml:"humanName" form:"humanName" query:"humanName" validate:"required"`
+	PrinterName    string `json:"printerName" xml:"printerName" form:"printerName" query:"printerName" validate:"required"`
 	FileName       string `json:"fileName" xml:"fileName" form:"fileName" query:"fileName" validate:"required"`
 	FilamentType   string `json:"filamentType" xml:"filamentType" form:"filamentType" query:"filamentType" validate:"required"`
 	FilamentOwner  string `json:"filamentOwner" xml:"filamentOwner" form:"filamentOwner" query:"filamentOwner" validate:"required"`
@@ -16,14 +17,14 @@ func (p *Print) GenerateLabelZPL() string {
 
 	output += "\n\n^FX First Section with person name and print name"
 	output += "\n^CF0,50"
-	output += "\n^FO50,50^FDName:^FS"
+	output += "\n^FO60,50^FDName:^FS"
 	output += "\n^CFD,40"
-	output += "\n^FO200,60^FD" + p.HumanName + "^FS"
+	output += "\n^FO210,60^FD" + p.HumanName + "^FS"
 
 	output += "\n\n^CF0,50"
-	output += "\n^FO50,120^FDFile:^FS"
+	output += "\n^FO60,120^FDFile:^FS"
 	output += "\n^CFD,40"
-	output += "\n^FO180,130^FD" + p.FileName + "^FS"
+	output += "\n^FO190,130^FD" + p.FileName + "^FS"
 
 	output += "\n\n^FO50,200^GB1100,3,3^FS"
 
@@ -31,28 +32,33 @@ func (p *Print) GenerateLabelZPL() string {
 
 	output += "\n\n\n^FX Second Section with plastic type"
 	output += "\n^CF0,50"
-	output += "\n^FO50,250^FDFilament:^FS"
+	output += "\n^FO60,250^FDFilament:^FS"
 	output += "\n^CFD,40"
-	output += "\n^FO260,260^FD" + p.FilamentType + "^FS"
+	output += "\n^FO270,260^FD" + p.FilamentType + "^FS"
 
 	output += "\n\n^CF0,50"
-	output += "\n^FO50,320^FDType:^FS"
+	output += "\n^FO60,320^FDType:^FS"
 	output += "\n^CFD,40"
-	output += "\n^FO180,330^FD" + p.FilamentOwner + "^FS"
+	output += "\n^FO190,330^FD" + p.FilamentOwner + "^FS"
 
 	output += "\n\n^CF0,50"
-	output += "\n^FO50,390^FDWeight:^FS"
+	output += "\n^FO60,390^FDWeight:^FS"
 	output += "\n^CFD,40"
-	output += "\n^FO220,400^FD" + p.FilamentWeight + "^FS"
+	output += "\n^FO230,400^FD" + p.FilamentWeight + " g^FS"
 
 	output += "\n\n^FO50,470^GB1100,3,3^FS"
 
 	// ------------------------------------------------
 
-	output += "\n\n\n^FX Third Section with time and QR"
+	output += "\n\n\n^FX Third Section with printer, time and QR"
+
+	output += "\n^CF0,50"
+	output += "\n^FO60,520^FDPrinter:^FS"
+	output += "\n^CFD,40"
+	output += "\n^FO230,530^FD" + p.PrinterName + "^FS"
 
 	output += "\n^CFD,40"
-	output += "\n^FO50,820^FD" + p.Time + "^FS"
+	output += "\n^FO60,820^FD" + p.Time + "^FS"
 
 	output += "\n\n^FO900,600"
 	output += "\n^BQN,2,8"
